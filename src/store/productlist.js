cde35cda4212f8198513899c84a2847b019d0d3d
Akshay <akshayrajput102@gmail.com>
2020-11-10 15:24:43 +0530

    clean object value strings in productlist

--- src/store/productlist.js
+++ src/store/productlist.js
@@ -6,10 +6,7 @@ export default
 			image: 'https://cdn.shopify.com/s/files/1/2060/6331/products/GlitchReverseDucks.png?v=1603996974',
 			name: 'Glitch Shaker',
 			description: 'BlenderBottle® Quality meets EPIC GHOST Hockey-Inspired Colorways.',
-			details: "Official BlenderBottle® Brand Shaker
-					BlenderBottle® Wire Whisk Included
-					EPIC Hockey-Inspired Colorways
-					GHOST® Glitch Logo",
+			details: "Official BlenderBottle® Brand Shaker					\n BlenderBottle® Wire Whisk Included					EPIC Hockey-Inspired Colorways					GHOST® Glitch Logo",
 			price: 7,
 			type: 'Merchandise',
 			wishlisted: false
@@ -19,11 +16,7 @@ export default
 			image: 'https://cdn.shopify.com/s/files/1/2060/6331/products/BCAAV2LemonCrush.png?v=1601922160',
 			name: 'Ghost BCCA',
 			description: 'Although traditionally taken during a workout or intense training session, GHOST® BCAA can be enjoyed around the clock as a phenomenal performance alternative to sugary sports drinks or juices.',
-			details: "GHOST® Full Disclosure Label
-				All GHOST® products feature a 100% transparent label that fully discloses the dose of each active ingredient. Zero proprietary blends means you know what you’re getting in each and every scoop.
-
-				BCAAs 7G
-				GHOST® BCAA delivers a massive 7G Vegan Fermented 2:1:1 BCAAs (3500MG Leucine, 1750MG Isoleucine and 1750MG Valine) per scoop.",
+			details: "GHOST® Full Disclosure Label				All GHOST® products feature a 100% transparent label that fully discloses the dose of each active ingredient. Zero proprietary blends means you know what you’re getting in each and every scoop.				BCAAs 7G				GHOST® BCAA delivers a massive 7G Vegan Fermented 2:1:1 BCAAs (3500MG Leucine, 1750MG Isoleucine and 1750MG Valine) per scoop.",
 			price: 20,
 			type: 'Muscle Builder',
 			wishlisted: false
@@ -32,8 +25,8 @@ export default
 			id: 3,
 			image: 'https://cdn.shopify.com/s/files/1/2060/6331/products/Size_Warheads_SGA.png?v=1598245515',
 			name: 'Ghost Size',
-			description: "New school meets old school, clinically dosed, packed to the nines, transparent AF, muscle builder conceived to make gains faster than photoshop.',
-			details: 'New school (Creatine HCl) meets old school (Creatine Monohydrate) Creatine combination added to maximize efficacy, absorption and solubility. GHOST® Size contains the first ever "Full Yield Creatine" delivering 5G of actual yielded Creatine per scoop. You're right, most brands add 5G of Creatine, but because Creatine isn't 100% pure you're left with much less than 5G of actual delivered Creatine. At GHOST® we cherish every gram (and the gains that come along with them), so we've beefed up our Creatine (3.21G HCl & 2.84G Monohydrate) to ensure you're getting the FULL, DELIVERED 5G dose in every scoop.",
+			description: "New school meets old school, clinically dosed, packed to the nines, transparent AF, muscle builder conceived to make gains faster than photoshop.",
+			details: "New school (Creatine HCl) meets old school (Creatine Monohydrate) Creatine combination added to maximize efficacy, absorption and solubility. GHOST® Size contains the first ever \"Full Yield Creatine\" delivering 5G of actual yielded Creatine per scoop. You're right, most brands add 5G of Creatine, but because Creatine isn't 100% pure you're left with much less than 5G of actual delivered Creatine. At GHOST® we cherish every gram (and the gains that come along with them), so we've beefed up our Creatine (3.21G HCl & 2.84G Monohydrate) to ensure you're getting the FULL, DELIVERED 5G dose in every scoop.",
 			price: 23,
 			type: 'Muscle Builder',
 			wishlisted: false
@@ -73,10 +66,7 @@ export default
 			image: 'https://cdn.shopify.com/s/files/1/2060/6331/products/InfraredCutOff_Flatlay.png?v=1600724872',
 			name: 'Ghost Summer Tones Sleeveless Tee',
 			description: "The September Capsule “SUMMER TONES V2” and the Summer Tones V2 Sleeveless Tee released Thursday, September 24, exclusively online and at 12PM CT.",
-			details: "GHOST® Striped Logo
-					Sneaker inspired colorways
-					Unisex
-					Once it's gone, it's gone..",
+			details: "GHOST® Striped Logo					Sneaker inspired colorways					Unisex					Once it's gone, it's gone..",
 			price: 10,
 			type: 'Merchandise',
 			wishlisted: false
@@ -86,9 +76,7 @@ export default
 			image: 'https://cdn.shopify.com/s/files/1/2060/6331/products/Gray_Snapback.png?v=1583169259',
 			name: 'Ghost Logo Snapback Cap',
 			description: "The must-have GHOST® accessory.",
-			details: "Classic fitting snapback with flat brim
-					Raised front embroidery
-					Custom GHOST® branded interior lining",
+			details: "Classic fitting snapback with flat brim					Raised front embroidery					Custom GHOST® branded interior lining",
 			price: 8,
 			type: 'Merchandise',
 			wishlisted: false
@@ -98,10 +86,7 @@ export default
 			image: 'https://cdn.shopify.com/s/files/1/2060/6331/products/VoltTee_Back.png?v=1598540928',
 			name: 'Ghost Glitch Tee',
 			description: "The May Capsule “GLITCH” and the GLITCH Tee released Thursday, May 28, exclusively online and at 12PM CT.",
-			details: "Classic Tee
-					GHOST® Gamer inspired GHOST® Logo
-					Unisex
-					Once it's gone, it's gone... ",
+			details: "Classic Tee					GHOST® Gamer inspired GHOST® Logo					Unisex					Once it's gone, it's gone... ",
 			price: 15,
 			type: 'Merchandise',
 			wishlisted: false
@@ -111,9 +96,7 @@ export default
 			image: 'https://cdn.shopify.com/s/files/1/2060/6331/products/Sportiqe_FlatShots_HeaCrop.png?v=1588261452',
 			name: 'Ghost Cropped Hoodie',
 			description: "The GHOST® Women's Cropped Hoodie is a brand new, limited edition piece designed by the women of GHOST®, for the women of GHOST®.",
-			details: "Premium SPORTIQE® Brand French Terry
-					Tapered fit with a Flattering Raw Edge
-					High Quality Double Layered Oversized GHOST® Logo Print ",
+			details: "Premium SPORTIQE® Brand French Terry					Tapered fit with a Flattering Raw Edge					High Quality Double Layered Oversized GHOST® Logo Print ",
 			price: 18,
 			type: 'Merchandise',
 			wishlisted: false
@@ -123,10 +106,7 @@ export default
 			image: 'https://cdn.shopify.com/s/files/1/2060/6331/products/Sportiqe_FlatShots_NvyZip.png?v=1574898875',
 			name: 'Ghost Zip-up hoodie',
 			description: "Legends, last year you loved the new custom scuba hood design. So we brought it back. Say welcome back to the most versatile fleece in your arsenal.",
-			details: "Premium SPORTIQE® Brand French Terry
-					Upgraded snag-free zipper 
-					Tapered fit spec'd to accentuate your physique 
-					Embroidered left chest GHOST® Logo",
+			details: "Premium SPORTIQE® Brand French Terry					Upgraded snag-free zipper 					Tapered fit spec'd to accentuate your physique 					Embroidered left chest GHOST® Logo",
 			price: 35,
 			type: 'Merchandise',
 			wishlisted: false
@@ -136,9 +116,7 @@ export default
 			image: 'https://cdn.shopify.com/s/files/1/2060/6331/products/Sportiqe_FlatShots_InfCrew.png?v=1574899487',
 			name: 'Ghost Classic Crewneck',
 			description: "Legends get the best of both worlds with the GHOST® Classic Crewneck.",
-			details: "Premium SPORTIQE® Brand French Terry
-					Tapered fit spec'd to accentuate your physique
-					High Quality Double Layered Oversized GHOST® Print",
+			details: "Premium SPORTIQE® Brand French Terry					Tapered fit spec'd to accentuate your physique					High Quality Double Layered Oversized GHOST® Print",
 			price: 25,
 			type: 'Merchandise',
 			wishlisted: false
