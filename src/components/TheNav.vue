13594e1e63f6651b610baae19521c96cb4d47a65
Akshay <akshayrajput102@gmail.com>
2020-11-10 13:03:34 +0530

    Added empty components

---
+++ src/components/TheNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="maincontainer">
+    <span class="text-gray-700">This is navbar....</span>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheNav',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+a {
+  color: #42b983;
+}
+</style>
