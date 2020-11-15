fad4030f056996f731de1199bcd40f84a0839388
Akshay <akshayrajput102@gmail.com>
2020-11-15 21:58:23 +0530

    removed signup component from homeview

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <!-- <p>Param: {{$route.params.section}}</p> -->
-    <the-signup></the-signup>
     <home-top-slider></home-top-slider>
     <HomeProductList></HomeProductList>
     <home-contact-form></home-contact-form>
@@ -11,7 +10,6 @@
 
 <script>
 // @ is an alias to /src
-import TheSignup from '@/components/TheSignup.vue'
 import HomeTopSlider from '@/components/HomeTopSlider.vue'
 import HomeProductList from '@/components/HomeProductList.vue'
 import HomeContactForm from '@/components/HomeContactForm.vue'
@@ -24,7 +22,6 @@ export default {
     }
   },
   components: {
-    TheSignup,
     HomeTopSlider,
     HomeProductList,
     HomeContactForm,
