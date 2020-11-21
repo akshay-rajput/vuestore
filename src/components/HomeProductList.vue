4fc87540fbddbb633e71861537ccc95092a00159
Akshay <akshayrajput102@gmail.com>
2020-11-21 18:17:06 +0530

    Stylise product card on homepage

--- src/components/HomeProductList.vue
+++ src/components/HomeProductList.vue
@@ -1,7 +1,29 @@
 <template>
-    <div class="container mx-auto my-3 py-3 px-3 border border-orange-700">
-        <h2 class="my-2 text-2xl text-gray-700">Products</h2>
-        <div id="products" class="grid sm:grid-cols-3 gap-8 my-2">
+    <div class="container mx-auto my-3 py-3 px-3">
+        <div class="flex justify-between items-center">
+            <div class="section-heading">
+                <h2 class="my-2 text-4xl text-gray-700">Products</h2>
+            </div>
+            <div class="productlist-options">
+                <div class="filters">
+                    <label for="filter_products" class="bg-gray-400 border border-gray-400 hover:border-gray-500 p-2 rounded-r-none shadow text-gray-800" title="Filter Products">
+                        <span class="flaticon flaticon-filter font-semibold"></span>
+                        <!-- <span class="fa fa-filter"></span> -->
+                    </label>
+                    <div class="inline-block relative w-36 lg:w-64">
+                        <select name="filter_products" id="filter_products" class="appearance-none z-0 w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded-l-none shadow leading-tight focus:outline-none focus:shadow-outline">
+                            <option value="all">All Products</option>
+                            <option value="powder">Protien Powder</option>
+                            <option value="merchandise">Merchandise</option>
+                        </select>
+                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="products" class="grid sm:grid-cols-3 gap-12 my-2">
             <home-product-list-item v-for="item in productList" :key="item.id" :item=item ></home-product-list-item>
         </div>
     </div>
@@ -27,3 +49,13 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.productlist-options .flaticon::before{
+  font-size: 14px;
+  margin: 0px 0px 0px 0px;
+//   margin-top: 5px;
+//   padding-top: 5px;
+//   padding-bottom:0;
+}
+</style>
--- src/components/HomeProductListItem.vue
+++ src/components/HomeProductListItem.vue
@@ -1,12 +1,60 @@
 <template>
-    <div class="productcard">
-        <div class="">
-            <img :src=item.image alt="Product image" class="h-48 mx-auto">
+    <div class="productcard border border-gray-400 rounded-md">
+        <div class="productcard-imagewrap rounded-md bg-gray-100">
+            <img :src=item.image alt="Product image" class="productcard-image">
         </div>
-        Name: {{item.name}} <br>
-        Desc: {{item.description}} <br>
-        Price: {{item.price}} <br>
-        Wish: {{item.wishlisted}} <br>
+        <div class="productcard-info px-2 pb-4">
+            <div class="productcard-name py-1 mb-2">
+                <h4 class="truncate tracking-wider text-lg md:text-2xl" :title=item.name>{{item.name}}</h4>
+            </div>
+            <div class="productcard-description mb-2">
+                <p class="fade-text tracking-wide text-gray-600 text-justify">
+                    <span class=" text-gray-900 border-dashed border-b border-gray-400">Description:</span>
+                 {{item.description}}  
+                </p>
+            </div>
+            <div class="productcard-price-qty flex justify-between items-center">
+                <div class="productcard-price ">
+                    <small for="" class="text-gray-700 text-xs">Price</small>
+                    <h5 class="text-gray-700 py-1"><span class="text-gray-700 pr-1">$</span>{{item.price}}</h5>
+                </div>
+                <div class="productcard-quantity">
+                    <small class="text-gray-700 text-xs">Quantity</small>
+                    <div class="product-quantity-control mt-1">
+                        <!-- minus qty -->
+                        <button class="btn-quantity-minus">
+                            <span class="fa fa-minus"></span>
+                        </button>
+                        
+                        <input type="text" name="product_quantity" id="product_quantity" value="1" 
+                                class="text-xl leading-4 font-bold text-center focus:outline-none cursor-default" readonly>
+                        
+                        <!-- plus qty -->
+                        <button class="btn-quantity-plus">
+                            <span class="fa fa-plus"></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="productcard-buttons border-gray-400 border-t border-dashed flex justify-between">
+            <div class="">
+                <button class="btn-viewDetails">
+                    View Details >
+                </button>    
+            </div>
+            <div class="flex-grow">
+                <button class="btn-viewDetails w-full">
+                    Add to cart 
+                </button>    
+            </div>
+        </div> -->
+        <div class="productcard-buttons">
+            <button class="btn-addToCart">
+                Add to cart
+            </button>
+            <a href="javascript:void(0);" class="text-blue-600 hover:text-teal-600">View details <span class="fa fa-long-arrow-alt-right"></span></a>
+        </div>
     </div>
 </template>
 
@@ -21,12 +69,83 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+@import '../assets/_variables.scss';
+
 .productcard{
-    padding: 10px;
+    background: white;
     word-wrap: break-word;
-    border: 1px solid #ccc;
-    /* box-shadow: 0px 0px 20px #eee; */
-    border-radius: 5px;
+}
+.productcard-imagewrap{
+    height: 200px;
+    
+    .productcard-image{
+        height: 190px;
+        margin: auto;
+    }
+}
+.productcard-description{
+    height: 4.65rem;
+    overflow: hidden;
+    
+    > p.fade-text{
+        line-height: 1.5rem;
+        height: 4.65rem;
+        position: relative;
+    }
+    .fade-text:after {
+        content: "";
+        text-align: right;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        width: 20%;
+        height: 1.5rem;
+        background: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 80%);
+    }
+}
+
+.productcard-price h5{
+    font-size: 2rem;
+    line-height: 1.75rem;
+}
+.product-quantity-control{
+
+    #product_quantity{
+        width: 40px;    
+        border: 1px solid lighten($color: $dark, $amount: 50%);
+    }
+
+    .btn-quantity-minus, .btn-quantity-plus{
+        background: lighten($color: $dark, $amount: 50%);
+        color: white;
+        padding: 1px 4px;
+        font-size: 1.25rem;
+
+        &:hover{
+            background: $accent;
+        }
+    }
+    .btn-quantity-minus{
+        border-radius: 4px 0px 0px 4px;
+    }
+    .btn-quantity-plus{
+        border-radius: 0px 4px 4px 0px;
+    }
+}
+
+.btn-addToCart{
+    margin-right: 8px;
+    padding: 8px 16px;
+    background: $accent;
+    border: 1px solid $accent;
+    color: white;
+    border-radius: 0px 0px 0px 4px;
+
+    &:hover{
+        color: $accent;
+        background: transparent;
+        transition: all ease 0.35s;
+    }
 }
 </style>
