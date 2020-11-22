78230217f17819f58d0509f980a3753089ae3ad4
Akshay <akshayrajput102@gmail.com>
2020-11-22 18:48:26 +0530

    Footer finalversion

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,62 +1,30 @@
 <template>
-    <div class="footer mt-16">
-        <div class="container mx-auto px-3 pt-8 text-gray-500">
-            <div class="flex justify-between items-center">
-                <div class="mr-4">
-                    <a href="javascript:void(0);" class="">
-                        <img src="../assets/Ghost_Logo_grey.png" alt="Ghost Logo" class="h-16">
-                    </a>
-                </div>
-                <ul class="flex justify-center items-center">
-                    <li class="mr-4">
-                        <a href="javascript:void(0);" class="text-sm  ">
-                            Home
-                        </a>
-                    </li>
-                    <li class="">
-                        <router-link v-if="$route.name != 'Home'" :to="{path: '/#products'}" class=" text-sm  mr-4">
-                            Products
-                        </router-link>
-                    </li>
-                    <li class="">
-                        <router-link v-if="$route.name != 'Home'" :to="{path: '/#contact'}" class=" text-sm  mr-4">
-                            Contact
-                        </router-link>                
-                    </li>
+    <div class="footer">
+        <div class="container mx-auto px-3 pt-12 pb-0 text-gray-500">
+            <div class="flex justify-between items-center py-3">
+                <div class="author-links">
+                    <p class="text-gray-600 text-sm mb-2">Reach out to developer</p>
+                    <!-- Place this tag where you want the button to render. -->
+                    <div class="dev-buttons flex items-center">
+                        <a class="github-button" href="https://github.com/root-0" data-size="large" aria-label="Follow @root-0 on GitHub">Follow Me</a>
                     
-                    <li class="">
-                        <!-- if on homepage show these links instead to scroll to sections -->
-                        <a href="javascript:void(0);" v-if="$route.name == 'Home'" @click="gotosection('products')" class="text-sm  mr-4">
-                            Products 
+                        <a href="https://www.linkedin.com/in/akshay-rajput/" rel="noopener noreferrer" class="btn-linkedin">
+                            Connect | <span class="fab fa-linkedin text-"></span>
                         </a>
-                    </li>
-                    <li class="">
-                        <a href="javascript:void(0);" v-if="$route.name == 'Home'" @click="gotosection('contact')" class="text-sm  mr-4">
-                            Contact
-                        </a>
-                    </li>
-                </ul>
-                <ul class="flex justify-start items-center">
-                    <li class="mr-3">
-                        <a href="https://instagram.com" class="">
-                            <span class="fab fa-instagram"></span>
-                        </a>
-                    </li>
-                    <li class="mr-3">
-                        <a href="https://fb.com" class="">
-                            <span class="fab fa-facebook"></span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://twitter.com" class="">
-                            <span class="fab fa-twitter"></span>
-                        </a>
-                    </li>
-                </ul>
+                    </div>
+                </div>
+                <div class="site-credits">
+                    <span class="fa fa-code"></span> by 
+                    <h5 class="inline">
+                        <a href="https://www.linkedin.com/in/akshay-rajput/" target="_blank" title="Visit Linkedin profile"
+                            rel="noreferrer noopener" class="text-purple-400">Akshay</a>
+                    </h5>
+                </div>
             </div>
-            <div class="copyright text-center text-xs text-gray-600 pb-3">
-                <span class="font-semibold">Disclaimer: </span>All images & products featured belong to their respective copyright holders. This website is just for illustration purposes. 
-            </div>
+        </div>
+        
+        <div class="copyright text-center text-xs text-gray-500 p-3 border-t border-dashed border-gray-600">
+            <span class="font-semibold">Disclaimer: </span>All images & products featured belong to their respective copyright holders. This website is just for illustration purposes. 
         </div>
     </div>
 </template>
@@ -65,5 +33,27 @@
 @import '../assets/variables';
     .footer{
         background: $dark;
+
+        .btn-linkedin{
+            padding: 4px 8px;
+            font-size: 0.8rem;
+            line-height: 1rem;
+            border: slateblue 1px solid;
+            background: slateblue;
+            color: white;
+            margin-top: -6px;
+            margin-left: 10px;
+            border-radius: 4px;
+
+            &:hover{
+                background: teal;
+                border-color: teal;
+                transition: all ease 0.35s;
+            }
+
+            span{
+                font-size: 1rem;
+            }
+        }
     }
 </style>
