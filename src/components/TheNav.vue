5ff3b189ff7c7a4dd680e6812f10e6181f7c0978
Akshay <akshayrajput102@gmail.com>
2020-11-27 18:10:25 +0530

    getCartItemCount to display in navbar

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -25,11 +25,12 @@
               </div>
               
               <div class="accountlinks flex justify-center items-center" v-if="isLoggedIn">
-                <router-link to='/cart' tag="a" class="bg-gray-300 rounded-full p-2 mx-2" title="Go to Cart">
+                <router-link to='/cart' tag="a" class="relative bg-gray-300 rounded-full p-2 mx-2" title="Go to Cart">
                   <!-- <span class="fa fa-cart-arrow-down"></span> -->
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" width=24 height=24 viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                   </svg>
+                  <span class="absolute top-0 right-0 bg-purple-400 text-black px-2 rounded-full">{{getCartItemCount}}</span>
                 </router-link>
                 <a href="javascript:void(0);" class=" bg-gray-300 rounded-full p-2 mx-2" title="Account">
                   <!-- <span class="flaticon-user-1"></span> -->
@@ -162,6 +163,10 @@ export default {
   computed: {
     isLoggedIn(){
       return this.$store.getters.getLoginStatus;
+    },
+    getCartItemCount(){
+      console.log("getting cart count");
+      return this.$store.getters.getCartItems.length;
     }
   },
   components: {
