941366034a518e1bc9ba35cee1cbc451aa709ad6
Akshay <akshayrajput102@gmail.com>
2020-11-27 18:21:32 +0530

    Added marker for displaying no. of items in cart

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -30,7 +30,7 @@
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" width=24 height=24 viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                   </svg>
-                  <span class="absolute top-0 right-0 bg-purple-400 text-black px-2 rounded-full">{{getCartItemCount}}</span>
+                  <span class="cart-item-count">{{getCartItemCount}}</span>
                 </router-link>
                 <a href="javascript:void(0);" class=" bg-gray-300 rounded-full p-2 mx-2" title="Account">
                   <!-- <span class="flaticon-user-1"></span> -->
@@ -267,5 +267,21 @@ export default {
 .dropdown-menu .flaticon::before{
   font-size: 16px;
   margin-left: 6px;
+}
+
+.cart-item-count{
+  position: absolute;
+  top: -10px;
+  right: -5px;
+  font-size: 14px;
+  font-weight: bold;
+  color: white;
+  padding:1px 8px;
+  background: teal;
+  border-radius: 50%;
+
+  @media (max-width: 767px) {
+    font-size: 11px;
+  }
 }
 </style>
