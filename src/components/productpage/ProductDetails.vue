067e5c00563a9394d39f8e2a05cdaa6ff999c7ef
Akshay <akshayrajput102@gmail.com>
2020-11-29 21:44:24 +0530

    Created component to display product details

---
+++ src/components/productpage/ProductDetails.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="productpage">
+        <h1 class="">here is your product</h1>
+        <p class="">{{product}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        product: Object
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
